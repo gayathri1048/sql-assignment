b82bb8f4fd5e7da68df63aa706a9a343851e16a9
gayathri1048 <130963208+gayathri1048@users.noreply.github.com>
2023-05-07 23:35:19 +0530

    Update assignment2.sql

--- assignment2.sql
+++ assignment2.sql
@@ -1,5 +1,18 @@
 -- question 1
 
+-- Exercise 1 (5 marks):
+
+-- Create a database schema for the online consultation and therapy website. The database should contain the following tables:-
+
+-- Doctor table:- The table should contain the attributes such as id, name, specialization, etc.
+
+-- Appointments table:- The table should contain information related to appointments set up by doctors and patients.
+
+-- Patient table:- The table should contain information on patients.
+
+-- Reviews table:- This table should contain reviews posted by patients.
+
+
 
 create database consultation;
 create table doctor(doc_id int primary key not null,doc_name varchar(30) not null,specialization varchar(30) not null);
@@ -14,7 +27,28 @@ select * from reviews;
 
 
 
--- question2
+
+
+
+
+
+-- Exercise 2 (5 marks):
+
+-- Contact Table
+-- QUESTIONS:
+
+-- 1.Select all columns from the contact table where the active flag is 1
+
+-- 2.Deactivate contacts who are opted out 
+
+-- 3.Delete all the contacts who have the company name as ‘ABC’
+
+-- 4.Insert a new row with id as 658, name as ‘mili’, email as ‘mili@gmail.com’, the company as ‘DGH’, active flag as 1, opt-out flag as 1
+
+-- 5.Pull out the unique values of the company column 
+
+-- 6.Update name “mili” to “niti”.
+
 
 create table contact(id int not null,email varchar(30),fname varchar(30),lname varchar(30),company varchar(30),active_flag int,opt_out int);
 insert into contact values(123,'a@a.com','kian','seth','ABC',1,1),(133,'b@a.com','neha','seth','ABC',1,0),(234,'c@c.com','puru','malik','CDF',0,0),(342,'d@d.com','sid','maan','TEG',1,0);
@@ -34,7 +68,57 @@ update contact set fname='niti' where id=658;
 
 
 
--- 3 question
+
+
+
+Write a SQL query to find those customers with a grade less than 100. It should return cust_name, customer city, grade, salesman, and salesman city. The result should be ordered by ascending customer_id.
+
+
+Sample table: customer
+
+customer_id |   cust_name    |    city    | grade | salesman_id 
+
+-------------+----------------+------------+-------+-------------
+
+        3002 | Nick Rimando   | New York   |   100 |        5001
+
+        3007 | Brad Davis     | New York   |   200 |        5001
+
+        3005 | Graham Zusi    | California |   200 |        5002
+
+        3008 | Julian Green   | London     |   300 |        5002
+
+        3004 | Fabian Johnson | Paris      |   300 |        5006
+
+        3009 | Geoff Cameron  | Berlin     |   100 |        5003
+
+        3003 | Jozy Altidor   | Moscow     |   200 |        5007
+
+        3001 | Brad Guzan     | London     |       |        5005
+
+
+
+
+
+Sample table: salesman
+
+ salesman_id |    name    |   city   | commission 
+
+-------------+------------+----------+------------
+
+        5001 | James Hoog | New York |       0.15
+
+        5002 | Nail Knite | Paris    |       0.13
+
+        5005 | Pit Alex   | London   |       0.11
+
+        5006 | Mc Lyon    | Paris    |       0.14
+
+        5007 | Paul Adam  | Rome     |       0.13
+
+        5003 | Lauson Hen | San Jose |       0.12
+
+
 create table customer(customer_id int primary key not null,cust_name varchar(30),customer_city varchar(30),grade int,salesman_id int not null);
 insert into customer values(3002,'Nick Ramindo','New york',100,5001),(3007,'Brad Davis','New York',200,5001),(3005,'Graham Zusi','California',200,5001),(3008,'Julian Green','London',300,5002),(3004,'Fabin Johnson','Paris',300,5006),(3009,'Geoff Cameron','Berlin',100,5003),(3003,'Jozy Altidor','Mascow',200,5007),(3001,'Brad Guzan','London',null,5005);
 select * from customer;
